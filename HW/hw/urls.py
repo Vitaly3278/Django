42e4eee87deb70da5ae86044673789d30748a1f9
vital-nvl <vital-nvl@mail.ru>
2023-09-25 19:55:52 +0300

    HW2

--- HW/hw/urls.py
+++ HW/hw/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from HW1 import views
+from hw1 import views
 
 urlpatterns = [
     path('', views.index, name='index'),
--- HW/hw2/management/commands/create_user.py
+++ HW/hw2/management/commands/create_user.py
@@ -3,22 +3,8 @@ from hw2.models import User
 
 class Command(BaseCommand):
     help = "Create User"
-
-    def add_arguments(self, parser):
-        parser.add_argument("name", type=str, help="User Name")
-        parser.add_argument("email", type=str, help="User email")
-        parser.add_argument("password", type=str, help="User password")
-        parser.add_argument("phone", type=str, help="phone email")
-        parser.add_argument("address", type=str, help="User address")
-
     def handle(self, *args, **kwargs):
-        name = kwargs.get('name')
-        email = kwargs.get('email')
-        password = kwargs.get('password')
-        phone = kwargs.get('phone')
-        address = kwargs.get('address')
-
-        user = User(name=name, email=email, password=password, phone=phone, address=address)
+        user = User(name='Vitaly', email='vital-nvl@mail.ru', password='pass', phone='89610000123', address='Bryansk')
         user.save()
 
         self.stdout.write(f'{user}')
