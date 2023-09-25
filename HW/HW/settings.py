c22dcbd04d3daa7dddd9cdb6c643730c98806533
vital-nvl <vital-nvl@mail.ru>
2023-09-25 19:15:23 +0300

    HW2

--- HW/HW/settings.py
+++ HW/HW/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'HW1',
+    'HW2',
 ]
 
 MIDDLEWARE = [
--- HW/HW2/models.py
+++ HW/HW2/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    date_registered = models.DateField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+
+
+class Order(models.Model):
+
+
+    — связь
+    с
+    моделью «Клиент», указывает
+    на
+    клиента, сделавшего
+    заказ
+    — связь
+    с
+    моделью «Товар», указывает
+    на
+    товары, входящие
+    в
+    заказ
+    — общая
+    сумма
+    заказа
+    — дата
+    оформления
+    заказа
