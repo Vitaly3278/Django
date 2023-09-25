e25d385bd842b6ee7d1e9fd575b6e41c76b7f25b
vital-nvl <vital-nvl@mail.ru>
2023-09-25 19:58:38 +0300

    HW2

--- HW/hw2/models.py
+++ HW/hw2/models.py
@@ -7,6 +7,8 @@ class User(models.Model):
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
     date_registered = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return f'Username: {self.name}, email: {self.email},  phone: {self.phone}, address: {self.address}'
 
 
 class Product(models.Model):
@@ -15,6 +17,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.IntegerField()
     date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return f'Product name: {self.name}, price: {self.price},  description: {self.description}, quantity: {self.quantity}'
 
 
 class Order(models.Model):
