dc3b8051b74597ca1c4820989269c533c14e2744
vital-nvl <vital-nvl@mail.ru>
2023-10-01 18:26:02 +0300

    HW3

--- HW/hw/settings.py
+++ HW/hw/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hw1',
     'hw2',
+    'hw4'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+3'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
